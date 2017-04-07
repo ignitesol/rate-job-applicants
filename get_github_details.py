d3ea82c2a0c21a6de2cff6e6ad83c5b15191d325
bsrikant <srikant.bondugula@ignitesol.com>
2017-04-07 12:35:09 +0530

    error handling for github get requests

--- get_github_details.py
+++ get_github_details.py
@@ -13,103 +13,135 @@ import sys
 import os
 from pandas.io.json import json_normalize
 
+
 requests_session = requests.Session()
 
-def check_error_msg(resp_json):
-    if 'message' in resp_json:  
-        error_msg = resp_json.get('message','')
+
+def github_get_request(url, params={}):
+    '''Function to send requests to github and return the json part of the response
+    '''
+    # get request
+    resp = requests_session.get(url, params=params, headers=AUTH_HEADER)
+    # error handling - print error message and exit
+    if resp.status_code in [403,400,422]:
+        error_msg = resp.json().get('message','')
         print("\nError: '{}'.\n\nExiting.\n".format(error_msg))
         sys.exit(0)
     else:
-        return
+        return resp.json()
+
 
 def get_matching_users(search_string):
+    '''Find all the users matching the search string.
+    '''
     url = 'https://api.github.com/search/users'
+    # search for the  search_string matches in fullnames, logins, emails
     params = {'q':search_string,
               'in':['fullname','login','email'],
               'sort':'score',
               'order':'desc'}
-    resp = requests_session.get(url, params=params, headers=AUTH_HEADER)
-    resp_json = resp.json()
-    check_error_msg(resp_json)
+    # get resonse with list of matching users
+    resp_json = github_get_request(url=url, params=params)
+    # list of user id details (user_login, user_name, user_email)
     users_list = []
+    # get user profile for each of the matching users - necessary for getting email ids
     for i,user_details in enumerate(resp_json.get('items',[])):
         profile_url = 'https://api.github.com/users/' + user_details['login']
-        profile = requests_session.get(profile_url, headers=AUTH_HEADER).json()
-        check_error_msg(profile)
+        profile = github_get_request(profile_url)
+        # combine user details and user profile
         resp_json['items'][i] = {**user_details, **profile}
         users_list.append((profile['login'], profile['name'], profile['email']))
+    # return list of user profiles and list of user id details
     return resp_json, users_list
 
-def get_user_repos(login):
-    url = 'https://api.github.com/users/' + login + '/repos'
-    params = {'type':'all'}
-    resp = requests_session.get(url, params=params, headers=AUTH_HEADER)
-    resp_json = resp.json()
-    check_error_msg(resp_json)
-    return resp_json
 
 def parse_user_details(matching_users):
+    '''For each of the users matching the search string,
+    get all the user repos and parse the information to a dataframe.
+    '''
     repos_dfs = []
+    # for each user get all the user repos and parse info to dataframe
     for user in matching_users.get('items',[]):
         login = user.get('login','')
-        repos = get_user_repos(login)
+        repos_url = 'https://api.github.com/users/' + login + '/repos'
+        repos_params = {'type':'all'}
+        # get all repos for user
+        repos = github_get_request(url=repos_url, params=repos_params)
         print('\t{}, {}, {}, {:2d} repos'.format(user['name'],user['login'],user['email'],len(repos)))
+        # for each repo, get contirbution details for the user and parse the info to dataframe
         for repo in repos:
+            # json to dataframe using builtin pandas method
             df_repo = json_normalize(repo)
+            # contributions url
             contribs_url = repo['contributors_url']
+            # get list of all contributions to the rep
             try:
-                contribs_list = requests_session.get(contribs_url, headers=AUTH_HEADER).json()
+                contribs_list = github_get_request(contribs_url)
             except json.JSONDecodeError:
                 continue
-            check_error_msg(contribs_list)
-            contribs_dict = {contrib['login']:contrib.get('contributions',0)\
-                                for contrib in contribs_list}
+            # dict of {user:contributions} for the rep
+            contribs_dict = {contrib['login']:contrib.get('contributions',0) \
+                             for contrib in contribs_list}
+            # get users contribution %
             sum_contribs = sum(contrib for contrib in contribs_dict.values())
             try:
                 user_contrib = 100*contribs_dict[login]/sum_contribs
-            except:
+            except ZeroDivisionError:
                 user_contrib = 0
+            # add contribution and owner details to the repo dataframe
             df_repo['contribution %'] = user_contrib
             df_repo['contributions'] = contribs_dict.get(login,0)
             df_repo['owner'] = repo.get('owner',{}).get('login','')
+            # append user id details to the repo dataframe
             user_details = ['login','name','email','score']
             for item in user_details:
                 df_repo['user_' + item] = user.get(item,'')
+            # add it to the list of repo dataframes
             repos_dfs.append(df_repo)
+    # combine all the repo dataframes for each of the matching users
     df_all = pd.concat(repos_dfs, axis=0, ignore_index=True)
+    # convert datetime columns to datetime objects
     date_cols = ['updated_at','created_at', 'pushed_at']
     for col in date_cols:
         df_all[col] = pd.to_datetime(df_all[col],infer_datetime_format=True)
+    # return combined dataframe
     return df_all
 
 if __name__ == '__main__':
+    # check if auth token file exists, get token if it does
     if os.path.isfile('github_auth.py'):
         import github_auth
         AUTH_TOKEN = github_auth.AUTH_TOKEN
         AUTH_HEADER = {'Authorization':'token ' + AUTH_TOKEN}
+    # proceed without auth if there is no auth token file
     else:
-        print("\nRequires github authentication token.")
-        print("Store the token as AUTH_TOKEN in github_auth.py")
-        print("Trying without authentication. (will be rate limited; email-ids may not be available)")
+        print("Trying without authentication.(will be rate limited; email-ids may not be available)")
+        print("Store github AUTH_TOKEN in github_auth.py to avoid 'API rate limit exceeded...' issue")
         AUTH_HEADER = {}
+    # check if search string was provided as part of command line
     try:
         search_string = sys.argv[1]
     except IndexError:
         print('\nRequires search string')
         sys.exit(0)
+    # fields to output to excel file
     fields = ['user_name', 'user_login', 'user_email', 'user_score', 'full_name', 'owner',
               'html_url', 'language', 'updated_at', 'forks_count', 'stargazers_count',
               'contribution %', 'contributions']
     print("\nFinding users matching '{}' ...".format(search_string))
+    # find all users matchin the search string
     matching_users, users_list = get_matching_users(search_string)
     n_matches = len(users_list)
+    # exit if there are no matches
     if n_matches == 0:
         print("Found 0 users matching '{}'; exiting\n".format(search_string))
         sys.exit(0)
+    # get details of all the matching users, parse the details to dataframe, write it to excel file
     else:
-        print("Found {} users matching '{}'.\nFetching details ...".format(n_matches, search_string))
+        print("Found {} users matching '{}'.\nFetching details ...".format(n_matches,search_string))
+        # get all repos for the user and output to a dataframe
         tabulated_details = parse_user_details(matching_users)[fields]
+        # write dataframe to excel file
         file_name = search_string.replace(' ','_') + '_github.xlsx'
         tabulated_details.to_excel(file_name)
         print('Details saved to {}'.format(file_name),'\n')
