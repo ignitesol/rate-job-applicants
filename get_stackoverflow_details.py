9fa0f0f57a10a1dd288514653374023504127e3a
bsrikant <srikant.bondugula@ignitesol.com>
2017-04-14 17:19:43 +0530

    -

--- get_stackoverflow_details.py
+++ get_stackoverflow_details.py
@@ -5,9 +5,25 @@ Created on Fri Apr 14 16:08:54 2017
 
 @author: srikant
 """
-import stackauth
+
+import argparse
 import stackexchange
 
-KEY = 'Lxz7RUcxbNbRZuawTRGOLQ(('
+
 
 so = stackexchange.Site(stackexchange.StackOverflow)
+
+if __name__ == '__main__':
+    # get search_string and auth_key from command line arguments
+    parser = argparse.ArgumentParser("\npython3 get_github_details_pygithub.py")
+    parser.add_argument("search_string", type=str,
+                        help="name to search for in user's name/email/login fields")
+    parser.add_argument("auth_token", type=str, nargs="?",
+                        help="github authentication token (to avoid rate limitation)")
+    args = parser.parse_args()
+    search_string = args.search_string
+    auth_token = args.auth_token
+    # initialize so object
+    # find all users matching the search string
+    # fields to output to excel file
+    # get all details for mathing users and save specified fields to excel sheet
---
+++ stackoverflow_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 16:40:29 2017
+
+@author: srikant
+"""
+
+AUTH_KEY = 'Lxz7RUcxbNbRZuawTRGOLQ(('
