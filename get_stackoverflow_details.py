faa2116438093907bd1b01863ec5a01c738570a7
bsrikant <srikant.bondugula@ignitesol.com>
2017-04-14 16:31:06 +0530

    first commit for get_stackoverflow_details

---
+++ get_stackoverflow_details.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 16:08:54 2017
+
+@author: srikant
+"""
+import stackauth
+import stackexchange
+
+KEY = 'Lxz7RUcxbNbRZuawTRGOLQ(('
+
+so = stackexchange.Site(stackexchange.StackOverflow)
